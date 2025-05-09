ef8ed294d1e5bc26d072d370bb82ef1380f904ec
Juan Diego <diego-738@hotmail.com>
2025-05-09 04:03:12 -0700

    Se instalo Net6.0 para su correcto funcionamiento

--- NotificationAPI/obj/Debug/net6.0/NotificationAPI.AssemblyInfo.cs
+++ NotificationAPI/obj/Debug/net6.0/NotificationAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NotificationAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+17f344d23f4a75ee2f110991cf61e072e171548d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f2764f491236bdfa180bb7a61184aad896639a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("NotificationAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NotificationAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
